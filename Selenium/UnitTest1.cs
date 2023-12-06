d5030a2261188815106f6e7fa6b545e19eec4a29
macius02 <mkuzmicki2002@gmail.com>
2023-12-06 18:36:03 +0100

    changedtests

--- Selenium/UnitTest1.cs
+++ Selenium/UnitTest1.cs
@@ -1,17 +1,18 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using Xunit;
 
-namespace TestProject1
+namespace Selenium
 {
     public class UnitTest1
     {
-        private readonly IWebDriver _driver;
 
+        private readonly IWebDriver _driver;
 
         public UnitTest1()
         {
-            ChromeOptions options = new ChromeOptions();
+	    ChromeOptions options = new ChromeOptions();
 
             options.AddArguments("ignore-certificate-errors");
             _driver = new ChromeDriver(options);
@@ -20,7 +21,7 @@ namespace TestProject1
         [Fact]
         public void testRead()
         {
-            Thread.Sleep(5000);
+	    Thread.Sleep(5000);
             _driver.Navigate().GoToUrl("https://localhost:7093/Api/Create");
             var titleInput = _driver.FindElement(By.CssSelector("input[name='Book.Title']"));
             titleInput.SendKeys("Test Book Title");
@@ -44,8 +45,8 @@ namespace TestProject1
         [Fact]
         public void create()
         {
-
-            Thread.Sleep(5000);
+            
+	    Thread.Sleep(5000);
             _driver.Navigate().GoToUrl("https://localhost:7093/Api/Create");
             var titleInput = _driver.FindElement(By.CssSelector("input[name='Book.Title']"));
             titleInput.SendKeys("Test Book Title");
@@ -72,7 +73,7 @@ namespace TestProject1
         [Fact]
         public void UpdateTest()
         {
-            Thread.Sleep(5000);
+	    Thread.Sleep(5000);
             _driver.Navigate().GoToUrl("https://localhost:7093/Api/Create");
             var titleInput = _driver.FindElement(By.CssSelector("input[name='Book.Title']"));
             titleInput.SendKeys("Test Book Title");
@@ -115,13 +116,13 @@ namespace TestProject1
             Assert.Contains("Test Book Updated", lastBookTitle);
             _driver.Quit();
 
-
+            
         }
 
         [Fact]
         public void TestBookDeletion()
         {
-            Thread.Sleep(5000);
+	    Thread.Sleep(5000);
             _driver.Navigate().GoToUrl("https://localhost:7093/Api/Create");
             var titleInput = _driver.FindElement(By.CssSelector("input[name='Book.Title']"));
             titleInput.SendKeys("Test Book Title");
@@ -154,10 +155,11 @@ namespace TestProject1
 
             var finalBookCardCount = _driver.FindElements(By.ClassName("card")).Count;
 
-            Assert.Equal(initialBookCardCount - 1, finalBookCardCount);
+            Assert.Equal(initialBookCardCount-1, finalBookCardCount);
             _driver.Quit();
 
         }
+
 
     }
 }
